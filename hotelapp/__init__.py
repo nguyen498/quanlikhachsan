0936e4e1ea1f5f50555ec4b3562f430b86a38bc2
nguyen498 <nh77442@gmail.com>
2021-12-10 20:27:29 +0700

    update login logout user

--- hotelapp/__init__.py
+++ hotelapp/__init__.py
@@ -1,18 +1,13 @@
-import sys
-
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '812ey8ihbkxjoapi-210(&(U2(&(*&*('
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456789@localhost/hotelapp?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:nguyen498@localhost/hotelapp?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['PAGE_SIZE'] = 8
 
 db = SQLAlchemy(app=app)
 
 login_manager = LoginManager(app)
-
-if not '-m' in sys.argv:
-    from hotelapp import index
