46186fd32b490fe8b50797d34b1d5696e9b630cd
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2021-12-13 09:59:58 +0700

    add-data

--- hotelapp/__init__.py
+++ hotelapp/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '812ey8ihbkxjoapi-210(&(U2(&(*&*('
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/hotelapp?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:nguyen498@localhost/hotelapp?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['PAGE_SIZE'] = 3
 
