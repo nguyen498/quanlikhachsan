931ff7b8172f5aa7612e151849f8fe35725ccd62
onggiabayluon <livipst2@gmail.com>
2021-12-14 10:11:06 +0700

    Feat(UI): modify  hotel_details.html

--- hotelapp/__init__.py
+++ hotelapp/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '812ey8ihbkxjoapi-210(&(U2(&(*&*('
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/hotelapp?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456789@localhost/hotelapp?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['PAGE_SIZE'] = 3
 
@@ -14,3 +14,5 @@ db = SQLAlchemy(app=app)
 
 login_manager = LoginManager(app)
 
+if not '-m' in sys.argv:
+    from hotelapp import index
